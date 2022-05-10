d85c68b29afc8116c240b474857242d8472533d1
bidishaMondal7 <bidisha.tuli7@gmail.com>
2022-05-11 02:16:32 +0530

    Login form updated bootstrap linking issue

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+// import { Form } from 'react-bootstrap';
 import './Login.css';
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login-div">
+      <span>User ID</span>
+      <input type="text" name="user-id" />
+      <br />
+      <span>Password</span>
+      <input type="password" name="user-id" />
+      <br />
+      <button>Login</button>
+    </div>
+    // <Form>
+    //   <Form.Group className="mb-3" controlId="formBasicEmail">
+    //     <Form.Label>Email address</Form.Label>
+    //     <Form.Control type="email" placeholder="Enter email" />
+    //     <Form.Text className="text-muted">
+    //       We will never share your email with anyone else.
+    //     </Form.Text>
+    //   </Form.Group>
+    //   <Form.Group className="mb-3" controlId="formBasicPassword">
+    //     <Form.Label>Password</Form.Label>
+    //     <Form.Control type="password" placeholder="Password" />
+    //   </Form.Group>
+    //   <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    //     <Form.Check type="checkbox" label="Check me out" />
+    //   </Form.Group>
+    // </Form>
+  );
 }
