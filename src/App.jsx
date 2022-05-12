1fee88b3c152d54056962f7d9621ab41ff933626
Avijit Das <adavijit@yahoo.com>
2022-05-13 00:06:38 +0530

    added root context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login/Login';
+// import Login from './components/Login/Login';
+import HomeScreen from './containers/HomeScreen/HomeScreen';
+import LoginSignupContainer from './containers/LoginSignupContainer/LoginSignupContainer';
+import RootContext from './contexts/RootContext';
 
 function App() {
+  /*
+  Setting user as a Root State because we need user information through out the application.
+  Every component would need user information
+  */
+  const [user, setUser] = useState(null);
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <RootContext.Provider value={{ user, setUser }}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginSignupContainer />} />
+          <Route path="/home" element={<HomeScreen />} />
+        </Routes>
+      </Router>
+    </RootContext.Provider>
   );
 }
 
